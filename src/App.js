c8440ec46dd1bcbaa0631f04fca6b9f508dec7db
Kalpanath Majhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2023-04-20 21:39:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Css from "./styles.css";
+import React, { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue) {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
